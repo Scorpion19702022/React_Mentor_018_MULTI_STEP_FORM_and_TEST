a6b68c6b9a8e6ca87f1fd9473fcdc9c015d21834
skorpion <jmosinski@o2.pl>
2025-08-22 20:57:27 +0200

    added validation

--- src/Components/StepOne/StepOne.tsx
+++ src/Components/StepOne/StepOne.tsx
@@ -3,9 +3,9 @@ import type { StepOneTypes } from './Interfaces/StepOneTypes'
 import { useForm, SubmitHandler } from 'react-hook-form'
 import styles from './StepOne.module.css'
 
-interface initialProps {
-	nextStep: () => void
-}
+// interface initialProps {
+// 	nextStep: () => void
+// }
 
 const StepOne = () => {
 	const {
@@ -15,18 +15,38 @@ const StepOne = () => {
 		reset,
 	} = useForm<StepOneTypes>()
 
-	const onSubmit: SubmitHandler<StepOneTypes> = data => console.log(data)
+	const onSubmit: SubmitHandler<StepOneTypes> = data => {
+		console.log(data)
+		reset()
+	}
+
+	const validationRorm = {
+		firstName: {
+			required: 'błąd',
+			minLength: {
+				value: 3,
+				message: 'pole powinno posiadać 3 znaki',
+			},
+		},
+		lastName: {
+			required: 'błąd',
+			minLength: {
+				value: 3,
+				message: 'pole powinno posiadać 3 znaki',
+			},
+		},
+	}
 
 	return (
 		<section className={styles.wrapper}>
 			<form onSubmit={handleSubmit(onSubmit)}>
 				<div>
-					<label>wpisz</label>
+					<label>podaj imię:</label>
 					<input type='text' {...register('firstName')} />
 					{errors.firstName && <span>{errors.firstName}</span>}
 				</div>
 				<div>
-					<label>wpisz</label>
+					<label>podaj azwisko</label>
 					<input type='text' {...register('lastName')} />
 					{errors.lastName && <span>{errors.lastName}</span>}
 				</div>
