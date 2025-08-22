8953e194fb53fc3cb92a64e0d571135e49381d04
skorpion <jmosinski@o2.pl>
2025-08-22 21:38:47 +0200

    added styles

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,10 @@ import MultiStepForm from './Components/MultiStepForm/MultiStepForm'
 function App() {
 	return (
 		<main className={styles.wrapper}>
+			<div className={styles.header}>
+				<h1 className={styles.heading}>- Formularz -</h1>
+				<span className={styles.text_info}>wypełnij formularze wykorzystując trzy kroki i zobacz wynik końcowy</span>
+			</div>
 			<MultiStepForm />
 		</main>
 	)
