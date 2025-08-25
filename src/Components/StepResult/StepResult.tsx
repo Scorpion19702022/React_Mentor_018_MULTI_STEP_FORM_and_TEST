60c8d4341bded2c0fc4a99d9b79cea9ee6846f83
skorpion <jmosinski@o2.pl>
2025-08-25 19:56:49 +0200

    added content

--- src/Components/StepResult/StepResult.tsx
+++ src/Components/StepResult/StepResult.tsx
@@ -4,7 +4,36 @@ const StepResult = () => {
 	return (
 		<section className={styles.wrapper}>
 			<h2 className={styles.heading}>{`resultat`.toLocaleUpperCase()}</h2>
-			<div className={styles.box_list}></div>
+			<div className={styles.box_list}>
+				<span className={styles.list}>
+					{`imię`.toLocaleUpperCase()}
+					<span className={styles.result}>Jacek</span>
+				</span>
+				<span className={styles.list}>
+					{`imię`.toLocaleUpperCase()}
+					<span className={styles.result}>Jacek</span>
+				</span>
+				<span className={styles.list}>
+					{`imię`.toLocaleUpperCase()}
+					<span className={styles.result}>Jacek</span>
+				</span>
+				<span className={styles.list}>
+					{`imię`.toLocaleUpperCase()}
+					<span className={styles.result}>Jacek</span>
+				</span>
+				<span className={styles.list}>
+					{`imię`.toLocaleUpperCase()}
+					<span className={styles.result}>Jacek</span>
+				</span>
+				<span className={styles.list}>
+					{`imię`.toLocaleUpperCase()}
+					<span className={styles.result}>Jacek</span>
+				</span>
+			</div>
+			<div className={styles.box_btns}>
+				<button className={styles.btn}>cofnij</button>
+				<button className={styles.btn}>wyczyść</button>
+			</div>
 		</section>
 	)
 }
