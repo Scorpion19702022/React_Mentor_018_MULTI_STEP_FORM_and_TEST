4c35dfd77a1fe7b3701d2624da3af5e5e56ed6e1
skorpion <jmosinski@o2.pl>
2025-08-25 19:37:30 +0200

    added style

--- src/Components/StepResult/StepResult.tsx
+++ src/Components/StepResult/StepResult.tsx
@@ -2,7 +2,7 @@ import styles from './StepResult.module.css'
 
 const StepResult = () => {
 	return (
-		<section>
+		<section className={styles.wrapper}>
 			<h2 className={styles.heading}>Resultat</h2>
 		</section>
 	)
