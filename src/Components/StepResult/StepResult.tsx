ddd3b60ff237b220370536385a92d3cd9d2e2a37
skorpion <jmosinski@o2.pl>
2025-08-25 19:41:12 +0200

    added style

--- src/Components/StepResult/StepResult.tsx
+++ src/Components/StepResult/StepResult.tsx
@@ -3,7 +3,7 @@ import styles from './StepResult.module.css'
 const StepResult = () => {
 	return (
 		<section className={styles.wrapper}>
-			<h2 className={styles.heading}>Resultat</h2>
+			<h2 className={styles.heading}>{`resultat`.toLocaleUpperCase()}</h2>
 		</section>
 	)
 }
