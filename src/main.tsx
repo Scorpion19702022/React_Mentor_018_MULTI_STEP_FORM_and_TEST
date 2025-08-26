dbd82dbbd3fccdeec0da3cb0fa29c0fb41a1d335
skorpion <jmosinski@o2.pl>
2025-08-26 20:13:42 +0200

    added provider

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { MultiStepProvider } from './Components/Context/MultiStepContext.tsx'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+	<StrictMode>
+		<MultiStepProvider>
+			<App />
+		</MultiStepProvider>
+	</StrictMode>
 )
