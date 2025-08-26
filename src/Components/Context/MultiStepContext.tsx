b6a569910e8509b1d2e5558c2a5581b7c2463618
skorpion <jmosinski@o2.pl>
2025-08-26 20:32:30 +0200

    added function

--- src/Components/Context/MultiStepContext.tsx
+++ src/Components/Context/MultiStepContext.tsx
@@ -2,12 +2,14 @@ import { createContext, useState } from 'react'
 
 type initialStateType = {
 	step: number
+	handleNextStep: () => void
 }
 
 type MultiStepProviderType = { children: React.ReactNode }
 
 const initialState: initialStateType = {
-	step: 0,
+	step: 1,
+	handleNextStep: () => {},
 }
 
 const MultiStepContext = createContext(initialState)
@@ -15,7 +17,11 @@ const MultiStepContext = createContext(initialState)
 export const MultiStepProvider = ({ children }: MultiStepProviderType) => {
 	const [step, setStep] = useState<number>(1)
 
-	return <MultiStepContext.Provider value={{ step }}>{children}</MultiStepContext.Provider>
+	const handleNextStep = () => {
+		setStep(2)
+	}
+
+	return <MultiStepContext.Provider value={{ step, handleNextStep }}>{children}</MultiStepContext.Provider>
 }
 
 export default MultiStepContext
