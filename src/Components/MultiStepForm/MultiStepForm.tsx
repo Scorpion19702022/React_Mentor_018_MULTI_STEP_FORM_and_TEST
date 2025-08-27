a380671d174b0fc477c9473f820b48676d2901fc
skorpion <jmosinski@o2.pl>
2025-08-27 19:59:22 +0200

    corrected in compoennts

--- src/Components/MultiStepForm/MultiStepForm.tsx
+++ src/Components/MultiStepForm/MultiStepForm.tsx
@@ -11,10 +11,17 @@ const MultiStepForm = () => {
 
 	return (
 		<main className={styles.wrapper}>
-			<StepOne isStepOne={step} />
-			<StepTwo isStepTwo={step} />
-			<StepThree />
-			<StepResult />
+			<div className={styles.box_heading}>
+				<h2>{`krok pierwszy`.toLocaleUpperCase()}</h2>
+				<h2>{`krok drugi`.toLocaleUpperCase()}</h2>
+				<h2>{`krok trzeci`.toLocaleUpperCase()}</h2>
+			</div>
+			<div className={styles.box_steps}>
+				<StepOne isStepOne={step} />
+				<StepTwo isStepTwo={step} />
+				<StepThree />
+				<StepResult />
+			</div>
 		</main>
 	)
 }
