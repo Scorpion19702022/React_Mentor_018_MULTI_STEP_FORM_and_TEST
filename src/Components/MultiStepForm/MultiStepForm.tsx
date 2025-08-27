413586a6cc0ebe4e19e536bd8b36de26e59927d7
skorpion <jmosinski@o2.pl>
2025-08-27 20:22:49 +0200

    added style

--- src/Components/MultiStepForm/MultiStepForm.tsx
+++ src/Components/MultiStepForm/MultiStepForm.tsx
@@ -12,9 +12,9 @@ const MultiStepForm = () => {
 	return (
 		<main className={styles.wrapper}>
 			<div className={styles.box_heading}>
-				<h2 className={styles.heading_step_one}>{`krok pierwszy`.toLocaleUpperCase()}</h2>
-				<h2 className={styles.heading_step_two}>{`krok drugi`.toLocaleUpperCase()}</h2>
-				<h2 className={styles.heading_step_three}>{`krok trzeci`.toLocaleUpperCase()}</h2>
+				<h2 className={styles.heading_step_one_active}>{`krok pierwszy`.toLocaleUpperCase()}</h2>
+				<h2 className={styles.heading_step_two_active}>{`krok drugi`.toLocaleUpperCase()}</h2>
+				<h2 className={styles.heading_step_three_active}>{`krok trzeci`.toLocaleUpperCase()}</h2>
 			</div>
 			<div className={styles.box_steps}>
 				<StepOne isStepOne={step} />
