c72f103cf9ed34334f6243080a1dc2b691214cfa
skorpion <jmosinski@o2.pl>
2025-08-31 17:47:47 +0200

    added function

--- src/Components/StepTwo/StepTwo.tsx
+++ src/Components/StepTwo/StepTwo.tsx
@@ -31,7 +31,7 @@ const StepTwo: React.FC<initialProps> = ({ stepTwo, stepEndTwo, backStep, addDat
 	const onSubmit: SubmitHandler<StepTwoTypes> = ({ age, gender }) => {
 		stepEndTwo()
 		addDateStepTwo(age, gender)
-		if (isClean) {
+		if (!isClean) {
 			reset()
 		}
 	}
